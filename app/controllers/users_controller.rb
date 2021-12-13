b4eb07704a365168bc9f1afd8b5c85daf71432bc
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2021-12-13 01:01:33 +0000

    booksからuserに飛ばす画像リンクが完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = Book.where(user_id: @user)
 
   end
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c29af62a8ba10b72c23e58c8f2786c06367229588e3e4b4b536f02367857390864dd9ae8fc9e231f6f80011e7ff500f2764bab773b80e255c68740b74eb9875d'
