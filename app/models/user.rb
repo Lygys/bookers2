7125e312340c6a232cb755f42d64ea31cc11b41c
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2021-12-13 15:12:32 +0000

    カラムのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   attachment :profile_image
-  validates :name, length: { minimum: 2 }
+  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
 end
